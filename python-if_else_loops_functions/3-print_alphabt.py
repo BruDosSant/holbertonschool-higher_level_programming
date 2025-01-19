dbedb8fefd7ab2824cbc2f2b8f5c28c15d5c81dd
BruDosSant <brudossant@gmail.com>
2025-01-19 17:11:59 +0000

    abcdario

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 i = 97
-print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in ['q', 'e']), end="")
+print(
+    "".join(
+        "{}".format(chr(i)) for i in range(97, 123)
+        if chr(i) not in ['q', 'e']
+    ),
+    end=""
+)
