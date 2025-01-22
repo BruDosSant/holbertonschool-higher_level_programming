346c5e90e3ab785906ebf1064b02d14ed2c4e499
BruDosSant <brudossant@gmail.com>
2025-01-22 17:11:54 +0000

    anshe

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len (my_list):
+    elif idx > len(my_list):
         return None
     else:
-        for iLO in range (my_list):
-            if iLO == idx:
-                return my_list(iLO)
+       return my_list[idx]
