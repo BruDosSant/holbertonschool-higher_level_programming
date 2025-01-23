e3c06e000559e09539800576679f01472d3c6b24
BruDosSant <brudossant@gmail.com>
2025-01-23 17:39:33 +0000

    Ejercicio6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for f in matrix:
         for c in f:
-            print("{:d}".format(c), end=" ")
-        print("")
+            if c != f[-1]:
+                print("{:d}".format(c), end=" ")
+            else:
+                print("{:d}".format(c))
