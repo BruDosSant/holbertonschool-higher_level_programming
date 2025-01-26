615c5c2578c57d5bb7175df5e7cf59793b1a1975
BruDosSant <brudossant@gmail.com>
2025-01-26 19:32:55 +0000

    ejercicio2

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             else:
                 if isinstance(my_list_1[i], (int, float)) and \
                 isinstance(my_list_2[i], (int, float)):
+
                     try:
                         result.append(my_list_1[i] / my_list_2[i])
                     except ZeroDivisionError:
