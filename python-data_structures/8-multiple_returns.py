71fa777c59c8e0b90553d78651c7f2f53b4de295
BruDosSant <brudossant@gmail.com>
2025-01-26 06:39:03 +0000

    ejercicio8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     if sentence == "":
         return (0, None)
-    
+
     length = len(sentence)
     first_char = sentence[0]
-    
+
     return (length, first_char)
