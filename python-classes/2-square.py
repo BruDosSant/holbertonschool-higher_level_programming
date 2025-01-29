b1321dbd11ce6510729e7ebc27ee5cf09e666400
BruDosSant <brudossant@gmail.com>
2025-01-29 14:16:32 -0300

    buenas buenas el 0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
     """A simple example class"""
     def __init__(self, size=0):
         self._Square__size = size
-    if not isinstance(self._Square__size, int):
+    if not isinstance(_Square__size, int):
         raise TypeError ("size must be an integer")
-    if self._Square__size < 0:
+    if _Square__size < 0:
         raise ValueError ("size must be >= 0")
