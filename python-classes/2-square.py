0009afee9dc553bd359842fea7693467d197a879
BruDosSant <brudossant@gmail.com>
2025-01-29 14:20:33 -0300

    buenas buenas el 0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,8 +9,8 @@ no se que poner es un cuadradinho
 class Square:
     """A simple example class"""
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError ("size must be an integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
         self._Square__size = size
-    if not isinstance(_Square__size, int):
-        raise TypeError ("size must be an integer")
-    if _Square__size < 0:
-        raise ValueError ("size must be >= 0")
