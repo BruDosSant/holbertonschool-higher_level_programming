5c1ff2d99e5ba8f48dc4c98e13963805ef482240
BruDosSant <brudossant@gmail.com>
2025-01-30 10:56:25 -0300

    hola muchachos dia 2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,3 +14,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self._Square__size = size
+
+    def area(self):
+        return self._Square__size * self._Square__size
