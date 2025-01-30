0b5eeb4d1bfd32186035842cfe91beb793de18b8
BruDosSant <brudossant@gmail.com>
2025-01-30 11:58:09 -0300

    hola muchachos dia 2 ejercicio 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,15 +9,17 @@ no se que poner es un cuadradinho
 class Square:
     """A simple example class"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self._Square__size = size
 
     def area(self):
         return self._Square__size * self._Square__size
 
+    @property
+    def size(self)
+        """Getter para obtener el tamaño del cuadrado."""
+        return _Square__size
+
+    @size.setter
     def size(self, value):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
