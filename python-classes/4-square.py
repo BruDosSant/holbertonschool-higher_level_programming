b00ca0fc5b3695a81dfa32469d3f02b3ed4e5510
BruDosSant <brudossant@gmail.com>
2025-01-30 11:49:30 -0300

    hola muchachos dia 2 ejercicio 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+no se que poner es un cuadradinho
+
+"""
+
+
+class Square:
+    """A simple example class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        return self._Square__size * self._Square__size
+
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
