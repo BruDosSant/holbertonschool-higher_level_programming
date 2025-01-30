c15f1beb3a820164456526ccdcff8e1a4ebfa16a
BruDosSant <brudossant@gmail.com>
2025-01-30 14:20:16 -0300

    hola muchachos dia 2 ejercicio 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,7 +37,7 @@ class Square:
         """Setter para modificar la posición del cuadrado con validación."""
         if (not isinstance(value, tuple) or len(value) != 2 or
             not all(isinstance(num, int) and num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+                raise TypeError("position must be a tuple of 2 positive integers")
         self._Square__position = value
 
     def area(self):
