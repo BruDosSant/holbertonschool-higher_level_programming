f90a3a4566311c3883b90ff74eb376eed50fb589
BruDosSant <brudossant@gmail.com>
2025-01-30 12:21:55 -0300

    hola muchachos dia 2 ejercicio 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,18 +11,18 @@ class Square:
     def __init__(self, size=0):
         self._Square__size = size
 
-    @property
-    def size(self):
-        """Getter para obtener el tamaño del cuadrado."""
-        return _Square__size
+        @property
+        def size(self):
+            """Getter para obtener el tamaño del cuadrado."""
+            return _Square__size
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self._Square__size = size
+        @size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self._Square__size = size
 
     def area(self):
         return self._Square__size * self._Square__size
