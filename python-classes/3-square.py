bf581d4b489e28a079d62a4e2a522b8177e00531
BruDosSant <brudossant@gmail.com>
2025-01-30 10:37:47 -0300

    hola muchachos dia 2

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+no se que poner es un cuadradinho
+
+"""
+
+
+class Square:
+    """A simple example class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
