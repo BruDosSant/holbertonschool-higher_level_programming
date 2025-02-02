e9e8da4d8235587dcd7d9c1e17add5c8aa9939b6
BruDosSant <brudossant@gmail.com>
2025-02-02 15:16:08 -0300

    como estan muchachos

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -58,3 +58,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+    """Retorna una representación formal del rectángulo para poder recrearlo."""
+    return f"Rectangle({self.__width}, {self.__height})"
