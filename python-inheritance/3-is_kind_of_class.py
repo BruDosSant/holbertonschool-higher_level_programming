6a269dc116cf4eb38af918710a08e1a0157b0594
BruDosSant <brudossant@gmail.com>
2025-02-09 14:40:07 -0300

    ejercicio 2

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Función que devuelve True si el objeto es una instancia exacta de la
+clase especificada, de lo contrario, devuelve False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Verifica si obj es exactamente una instancia de a_class."""
+    return isinstance(obj, a_class)
