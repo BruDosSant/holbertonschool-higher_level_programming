785711f55c4a95ea81ec5e6c4591a40ecfeb1d37
BruDosSant <brudossant@gmail.com>
2025-02-09 14:48:54 -0300

    ejercicio 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Función que devuelve True si el objeto es una instancia exacta de la
+clase especificada, de lo contrario, devuelve False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Verifica si obj es exactamente una instancia de a_class."""
+    if type(obj) is a_class
+        return False
+    else
+        return True
