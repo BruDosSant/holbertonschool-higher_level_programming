09499a81e7047a267130a2d162a4368ad4113ee4
BruDosSant <brudossant@gmail.com>
2025-02-09 16:02:43 -0300

    ejercicio 10

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -18,9 +18,9 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.integer_validator("size", size)  # Llamar a la validación de Rectangle
-        self.__size = size  # Tamaño privado
-        super().__init__(size, size)  # Llamar al constructor de Rectangle con el mismo tamaño para width y height
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """
@@ -30,12 +30,14 @@ class Square(Rectangle):
 
     def __str__(self):
         """
-        Return a string representation of the square in the format [Square] <size>/<size>.
+        Return a string representation of the square
+         in the format [Square] <size>/<size>.
         """
         return f"[Square] {self.__size}/{self.__size}"
 
     def __repr__(self):
         """
-        Return a string representation of the square that allows recreation using eval().
+        Return a string representation of the square that allows
+         recreation using eval().
         """
         return f"Square({self.__size})"
