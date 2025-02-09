1acd14d73e34f281d5fc13e21d1e464b4f6a66fe
BruDosSant <brudossant@gmail.com>
2025-02-09 15:10:01 -0300

    ejercicio 6

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,3 +11,9 @@ class BaseGeometry:
     def area(self):
         """Imprime la lista en orden ascendente sin modificar la original."""
         raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Imprime la lista en orden ascendente sin modificar la original."""
+        if isinstance(value, int):
+            raise ValueError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
