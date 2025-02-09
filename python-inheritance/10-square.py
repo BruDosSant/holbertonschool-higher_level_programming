0b1f1f60783bfc85009a24579e59dd324815b8d1
BruDosSant <brudossant@gmail.com>
2025-02-09 15:52:13 -0300

    ejercicio 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+no se que poner es un cuadradinho
+
+"""
+
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle): 
+   """Calcula el área del cuadrado."""
+    def __init__(self, size):
+        """Calcula el área del cuadrado."""
+        self.integer_validator("size", size)
+
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calcula el área del cuadrado."""
+        return self.__size * self.__size
