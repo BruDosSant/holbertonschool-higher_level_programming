fc165c7d852e38698594e81b84c8423d9d50543f
BruDosSant <brudossant@gmail.com>
2025-02-09 03:44:55 +0000

    ejercicio 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+no se que poner es un cuadradinho
+
+"""
+
+
+def print_sorted(self):
+    """Clase que define un rectángulo con ancho y alto."""
+
+    messi = sorted(self)
+    return messi
