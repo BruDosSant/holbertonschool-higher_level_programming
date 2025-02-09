cc807c8dcd083c839901b0746e85745c9871e52b
BruDosSant <brudossant@gmail.com>
2025-02-09 14:37:37 -0300

    ejercicio 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Función que devuelve True si el objeto es una instancia exacta de la clase,
-de lo contrario, devuelve False.
+Función que devuelve True si el objeto es una instancia exacta de la
+clase especificada, de lo contrario, devuelve False.
 """
 
 
 def is_same_class(obj, a_class):
     """Verifica si obj es exactamente una instancia de a_class."""
-    return type(obj) == a_class
+    return isinstance(obj, a_class) and type(obj) is a_class
