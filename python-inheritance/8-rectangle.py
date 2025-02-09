f3c39f031b3e1df9ca01396b3d15bd5da9f76dbc
BruDosSant <brudossant@gmail.com>
2025-02-09 15:47:46 -0300

    ejercicio 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,10 +6,12 @@ no se que poner es un cuadradinho
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 class Rectangle(BaseGeometry):
-    """Clase que extiende list con un método para imprimir la lista ordenada"""
+    """Clase base para geometría con métodos para área y validación de int."""
     def __init__(self, width, height):
-        # Validar los valores de width y height usando integer_validator
+        """Lanza una excepción indicando que el área no está implementada."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
