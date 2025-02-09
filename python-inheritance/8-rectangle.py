05fa5260464ce5368bc291766650a1d07aef5bce
BruDosSant <brudossant@gmail.com>
2025-02-09 15:48:14 -0300

    ejercicio 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ no se que poner es un cuadradinho
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Clase base para geometría con métodos para área y validación de int."""
     def __init__(self, width, height):
