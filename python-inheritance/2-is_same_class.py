9117659d4e324092690e405594d86a16000a0ef3
BruDosSant <brudossant@gmail.com>
2025-02-09 04:44:32 +0000

    ejercicio 1

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ no se que poner es un cuadradinho
 
 def is_same_class(obj, a_class):
     """comprueba si un objeto cumple con una instancia de una clase"""
-    return type(obj) == a_class
+    return isinstance(obj, a_class)
