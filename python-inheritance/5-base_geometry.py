dac608642eb8458a37c3e56fc99909f77790614a
BruDosSant <brudossant@gmail.com>
2025-02-09 14:56:49 -0300

    ejercicio 4

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+no se que poner es un cuadradinho
+
+"""
+
+
+class MyList(list):
+    """Clase que extiende list con un método para imprimir la lista ordenada"""
+    pass
