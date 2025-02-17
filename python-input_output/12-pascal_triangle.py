19758601ac88fd5dab1a009b2995265c388d1eef
BruDosSant <brudossant@gmail.com>
2025-02-17 02:25:33 +0000

    Impresionante

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+increible
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Primera fila
+
+    for i in range(1, n):
+        prev_row = triangle[-1]  # Última fila generada
+        new_row = [1]  # Comienza con 1
+        
+        for j in range(1, len(prev_row)):  
+            new_row.append(prev_row[j - 1] + prev_row[j])  # Suma de los de arriba
+        
+        new_row.append(1)  # Termina con 1
+        triangle.append(new_row)  # Agrega la nueva fila
+    
+    return triangle
