ecc0dbf3496e4383806e1e0bc34e5ceaa1a5147b
BruDosSant <brudossant@gmail.com>
2025-03-05 09:38:47 -0300

    messi

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,19 +1,45 @@
-#!/usr/bin/python3
 import http.server
 import socketserver
+import json
 
-class FrogHandler(http.server.BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-
-        self.wfile.write(b"Hello, this is a simple API!")
-
-host = "localhost"
 PORT = 8000
 
-with socketserver.TCPServer((host, PORT), FrogHandler) as httpd:
-    print(f"Serving on {host}:{PORT}")
+class run(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == "/info":
+            self.send_response(200)
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+with socketserver.TCPServer(("", PORT),run ) as httpd:
+    print("serving at port", PORT)
     httpd.serve_forever()
