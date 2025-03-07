e659db345b672c09138e34d251d952c2252c5130
BruDosSant <brudossant@gmail.com>
2025-03-07 02:31:28 -0300

    Ejercicio 0

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Crear usuario con todos los privilegios
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
