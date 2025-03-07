dd6e23a5474e1acc4da8f8fa8779daf3eb203894
BruDosSant <brudossant@gmail.com>
2025-03-07 02:55:36 -0300

    Ejercicio 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Listar todas las ciudades con su nombre y el nombre del estado
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
