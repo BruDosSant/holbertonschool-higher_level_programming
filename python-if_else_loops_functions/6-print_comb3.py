947fc8da1bac1f2721d5076f541760eeef214a32
root <root@PCera.>
2025-05-14 16:41:02 -0300

    Listo pa

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-for a in range(10):
-    for b in range(10):
+c = 1
+for a in range(9):
+    for b in range(c,10):
         if a == b:
             continue
         if a == 8 and b == 9:
             print("{}{}".format(a, b))
         else:
             print("{}{}, ".format(a, b), end="")
+    c += 1
