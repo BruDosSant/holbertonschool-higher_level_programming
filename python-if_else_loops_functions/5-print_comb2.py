8a8e5d6b2d023a7cb5fa42b0faa631d933d1d2f0
root <root@PCera.>
2025-05-14 16:03:05 -0300

    Listo pa

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for num in range((0), (100)):
-    if num >= 0 and num <= 9:
-        print("0{}, ".format(num), end="")
     if num == 99:
         print("{}".format(num))
     else:
-        print("{}, ".format(num), end="")
+        print("{:02}, ".format(num), end="")
