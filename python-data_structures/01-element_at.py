229dbc8613b6cefa34cb17516f03b71840eaef6a
root <root@PCera.>
2025-05-18 15:25:16 -0300

    Listo pa

---
+++ python-data_structures/01-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if index < 0:
+        return None
+    if index >= len(my_list):
+        return None
+    else:
+        return my_list[index]
