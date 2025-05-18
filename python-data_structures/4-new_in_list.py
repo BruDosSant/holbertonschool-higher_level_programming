b24c07d15f39901b39b42ba8ad17d85b1a563493
root <root@PCera.>
2025-05-18 15:54:10 -0300

    Listo pa

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    messi = my_list
+    if idx < 0:
+        return messi
+    if idx >= len(messi):
+        return messi
+    else:
+        messi [idx] = element
+        return messi
