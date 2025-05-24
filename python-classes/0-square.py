899f1593b3a0dd874883e8f5232ab9d0d9342211
root <root@PCera.>
2025-05-24 20:22:58 -0300

    clase cuadradito undecimo intento de memoria

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+0-square.py
+This module defines an empty class named `square`.
+It serves as a placeholder for future development.
+"""
+
+
 class square():
     """
     Doc de class, no hace nada
     """
-    pass
-    """
-    pase pa
-    """
+        pass
