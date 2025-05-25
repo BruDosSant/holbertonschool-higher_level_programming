1183cd94978260f25d0b0756c54e42518c708414
root <root@PCera.>
2025-05-25 00:38:53 -0300

    intento 1 ejercicio dos

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
++#!/usr/bin/python3
+"""
+1-square.py
+"""
+
+
+class Square:
+    """
+    Class Square that defines a square by its size.
+    """
+    def __init__(self, size):
+        """
+        inicializa el atributo size.
+        """
+        self.size = size
