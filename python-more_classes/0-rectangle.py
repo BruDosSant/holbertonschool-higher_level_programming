0ceba6caf491e8d19a7a52a4259af84704f03ea6
root <root@PCera.>
2025-05-25 15:57:20 -0300

    prueba 1 ejercicio 1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-rectangle.py
+This module defines an empty class named `rectangle`.
+It serves as a placeholder for future development.
+"""
+
+
+class rectangle():
+    """
+    Doc de class, no hace nada
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,4 @@
+ls
+aaaa
+p
+t
