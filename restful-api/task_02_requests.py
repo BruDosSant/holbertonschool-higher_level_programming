2d59f5db6d5d593eed310fd4e9f4dd0b9fc888b8
BruDosSant <brudossant@gmail.com>
2025-06-11 20:18:50 -0300

    anashe

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,2 +1,14 @@
 import requests
 import csv
+
+
+def fetch_and_print_posts():
+    url = 'https://jsonplaceholder.typicode.com'
+    response = requests.get(url)
+
+    print(f'Status Code: {response.status_codes}')
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
