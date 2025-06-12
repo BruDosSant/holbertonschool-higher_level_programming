e38ec86fceaab982899e49fd69b5fa4dbe547fb8
BruDosSant <brudossant@gmail.com>
2025-06-11 21:29:03 -0300

    anashe

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,7 +17,6 @@ def fetch_and_save_posts():
 
     if response.status_code == 200:
         posts = response.json()
-
         post_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
         
         with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
