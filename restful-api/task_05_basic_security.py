a9e59fe6babd55d15694e31864565206a89f34a5
BruDosSant <brudossant@gmail.com>
2025-06-22 18:40:42 -0300

    casos de prueba unittest

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -78,4 +78,4 @@ def handle_needs_fresh_token_error(jwt_header, jwt_payload):
     return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
