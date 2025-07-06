f9972c084a0b632d8aac60ef1e3891a1e42fd443
BruDosSant <brudossant@gmail.com>
2025-07-06 14:05:10 -0300

    Atrasados parte 6

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
 7-add_item.py
-Adds all arguments to a Python list,
-then saves them to a JSON file
+Adds all arguments to a Python list, then saves them to add_item.json
 """
-
 
 import sys
 import json
 from os.path import exists
 
-# Copiar la función de 5-save_to_json_file.py
+
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
 
-# Copiar la función de 6-load_from_json_file.py
+
 def load_from_json_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
+
 
 filename = "add_item.json"
 
