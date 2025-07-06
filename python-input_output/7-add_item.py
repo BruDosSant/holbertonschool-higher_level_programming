bcc2a4fdeec375935867fcd987f86b17de78295e
BruDosSant <brudossant@gmail.com>
2025-07-06 13:52:04 -0300

    Atrasados parte 4

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
 7-add_item.py
-Adds all arguments to a Python list, then saves them to add_item.json
+Adds all arguments to a Python list,
+then saves them to a JSON file
 """
+
 
 import sys
 from os.path import exists
@@ -11,11 +13,14 @@ from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
+# Intentar cargar el archivo si existe
 if exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
+# Agregar los nuevos argumentos
 items.extend(sys.argv[1:])
 
+# Guardar la lista actualizada
 save_to_json_file(items, filename)
